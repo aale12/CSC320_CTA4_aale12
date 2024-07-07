ddbd12ec86daeeaefe0899531949ad59d86c5c2e
Anthony Le <leanthony104@yahoo.com>
2024-07-07 17:52:51 -0500

    validation refactor

--- CTA4_Option2.java
+++ CTA4_Option2.java
@@ -4,48 +4,46 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CTA4_Option2 {
-    public static void main(String[] args) {
 
-        //prompting for 10 grades
-        Float gradesMax = null, gradesMin = null, gradesAverage, gradesSum = (float) 0.0;
+    public static void main(String[] args) {
+        //init variables
+        Float gradesMax = null, gradesMin = null, gradesAverage, userInput = null, gradesSum = (float) 0.0;
         Scanner scanner = new Scanner(System.in);
         ArrayList<Float> gradesArrayFloat = new ArrayList<>();
+        boolean invalidInput = true;
+
+        //prompting for 10 grades
         System.out.println("Enter 10 floating-point student grades");
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Enter grade " + i);
-            Float userInput = scanner.nextFloat();
+            while (invalidInput) {
+                try {
+                    System.out.println("Enter grade " + i);
+                    userInput = scanner.nextFloat();
+                    invalidInput = false;
+                } catch (Exception e) {
+                    System.out.println("You entered " + scanner.next());
+                    System.err.println("You did not enter a number, please try again!");
+                }
+            }
+            //tracking sum for average
             gradesArrayFloat.add(userInput);
             gradesSum += userInput;
             if (i == 1) {
                 gradesMax = userInput;
                 gradesMin = userInput;
-            }
+            } //assigning min
             else if (userInput < gradesMin) {
                 gradesMin = userInput;
-            } 
+            } //assigning max
             else if (userInput > gradesMax) {
                 gradesMax = userInput;
             }
+            invalidInput = true;
         }
-
-        // Float gradesMax = gradesArrayFloat.get(0), gradesMin = gradesArrayFloat.get(0);
-        // Float gradesSum = (float) 0.0;
-        // Float gradesAverage;
-        // for (Float grade: gradesArrayFloat) {
-        //     if (grade < gradesMin) {
-        //         gradesMin = grade;
-        //     }
-        //     if (grade > gradesMax) {
-        //         gradesMax = grade;
-        //     }
-        //     gradesSum += grade;
-        //     System.out.println(grade);
-        // }
         gradesAverage = gradesSum / gradesArrayFloat.size();
         System.out.println("Maximum is " + gradesMax);
         System.out.println("Minumum is " + gradesMin);
         System.out.println("Average is " + gradesAverage);
-
         scanner.close();
     }
 }
